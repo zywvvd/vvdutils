2c843ec313bf2064893ef2c012a6b3675d090331
vvd <zywvvd@mail.ustc.edu.cn>
2024-05-20 11:53:04 +0800

    update-utils

--- lib/processing/math_processing.py
+++ lib/processing/math_processing.py
@@ -273,3 +273,24 @@ def fit_gaussian_2D(point_xy_list):
 
     rho = ((point_array[:, 0] * point_array[:, 1]).mean() - mean_x * mean_y) / (sigma_x * sigma_y)
     return mean_x, mean_y, sigma_x, sigma_y, rho
+
+# 计算距离
+def getDistance(latA, lonA, latB, lonB):
+    ra = 6378140  # 赤道半径
+    rb = 6356755  # 极半径
+    flatten = (ra - rb) / ra  # Partial rate of the earth
+    # change angle to radians
+    radLatA = math.radians(latA)
+    radLonA = math.radians(lonA)
+    radLatB = math.radians(latB)
+    radLonB = math.radians(lonB)
+ 
+    pA = math.atan(rb / ra * math.tan(radLatA))
+    pB = math.atan(rb / ra * math.tan(radLatB))
+    x = math.acos(math.sin(pA) * math.sin(pB) + math.cos(pA) * math.cos(pB) * math.cos(radLonA - radLonB))
+    c1 = (math.sin(x) - x) * (math.sin(pA) + math.sin(pB)) ** 2 / math.cos(x / 2) ** 2
+    c2 = (math.sin(x) + x) * (math.sin(pA) - math.sin(pB)) ** 2 / math.sin(x / 2) ** 2
+    dr = flatten / 8 * (c1 - c2)
+    distance = ra * (x + dr)
+    distance = round(distance , 4)
+    return distance
