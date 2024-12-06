931c7afa1fe93dead1e4560cd9f449dd0cd0f90b
YiweiZhang <zywvvd@mail.ustc.edu.cn>
2024-12-06 09:37:20 +0800

    update-utils

--- lib/processing/onnx_processing.py
+++ lib/processing/onnx_processing.py
@@ -4,7 +4,31 @@
 import numpy as np
 import cv2
 from skimage.transform import resize
+from ..utils import get_gpu_str_as_you_wish
 
+def get_gpu_device_count(verbose=0):
+    try:
+        import pynvml
+    except Exception as e:
+        print('can not import pynvml.', e)
+        print('please make sure pynvml is installed correctly.')
+        print('a simple pip install nvidia-ml-py3 may help.')
+        print('now a 0 will be return')
+        return '0', [0]
+
+    try:
+        # 初始化工具
+        pynvml.nvmlInit()
+    except Exception as e:
+        print('pynvml.nvmlInit failed:', e)
+        print('now a 0 will be return')
+        return '0', [0]
+    # 驱动信息
+    if verbose:
+        print("GPU driver version: ", pynvml.nvmlSystemGetDriverVersion())
+    # 获取Nvidia GPU块数
+    gpuDeviceCount = pynvml.nvmlDeviceGetCount()
+    return gpuDeviceCount
 
 class OnnxSimpleRelease:
     mean=[
@@ -19,11 +43,24 @@ class OnnxSimpleRelease:
         57.375,
     ]
 
-    def __init__(self, model_path, specific_wh=None, mean=None, std=None, model_type='float32', gpu=True):
+    def __init__(self, model_path, specific_wh=None, mean=None, std=None, model_type='float32', gpu=True, device_id=None):
         import onnxruntime
+        try:
+            import pynvml
+        except Exception as e:
+            print('can not import pynvml.', e)
+            print('please make sure pynvml is installed correctly.')
+            print('a simple pip install nvidia-ml-py3 may help.')
+            print('now a 0 will be return')
+            raise RuntimeError("Onnx releaser need pynvml --- pip install nvidia-ml-py3")
+
+        self.gpuDeviceCount = get_gpu_device_count()
+        if self.gpuDeviceCount < 1:
+            gpu = False
 
         if mean is not None:
             self.mean = mean
+
         if std is not None:
             self.std = std
 
@@ -36,13 +73,25 @@ class OnnxSimpleRelease:
 
         assert model_type in ['float32', 'float16'], "model_type must be 'float32' or 'float16'"
         self.model_type = model_type
+        self.device_id = device_id
 
         # 加载模型
         if gpu:
+            CUDAExecutionProvider = {
+                "cudnn_conv_algo_search": "DEFAULT",
+                "cudnn_conv_use_max_workspace": '1'
+            }
+            if device_id is not None:
+                CUDAExecutionProvider.update({'device_id': str(device_id)})
+            else:
+                gpu_index_str, gpu_index_picked_list = get_gpu_str_as_you_wish(1)
+                CUDAExecutionProvider.update({'device_id': gpu_index_str})
+
+            print(f"gpu ONNX runtime init: {CUDAExecutionProvider}.")
             self.ort_session = onnxruntime.InferenceSession(model_path, providers=[
-                        ("CUDAExecutionProvider", {"cudnn_conv_algo_search": "DEFAULT", "cudnn_conv_use_max_workspace": '1'})
-                    ])
+                        ("CUDAExecutionProvider", CUDAExecutionProvider)])
         else:
+            print(f"cpu ONNX runtime init.")
             self.ort_session = onnxruntime.InferenceSession(model_path)
         self.input_name = self.ort_session.get_inputs()[0].name
 
