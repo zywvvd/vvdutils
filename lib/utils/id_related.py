6ce15bae853431ce07a6883886833f7116547867
YiweiZhang <zywvvd@mail.ustc.edu.cn>
2025-08-08 09:49:07 +0800

    update-utils

--- lib/utils/id_related.py
+++ lib/utils/id_related.py
@@ -2,6 +2,7 @@ import uuid
 import hashlib
 import time
 import os
+import threading 
 
 
 def create_uuid():
@@ -34,7 +35,6 @@ class Snowflake:
     snowflake = Snowflake(worker_id, data_center_id)  
     snowflake.next_id()
     '''
-
     def __init__(self, worker_id, data_center_id):  
         ### 机器标识ID
         self.worker_id = worker_id  
@@ -44,22 +44,29 @@ class Snowflake:
         self.sequence = 0  
         ### 时间戳
         self.last_timestamp = -1  
+        self.lock = threading.Lock()  # 线程锁
   
-    def next_id(self):  
-        timestamp = int(time.time() * 1000)  
-        if timestamp < self.last_timestamp:  
-            raise Exception("Clock moved backwards. Refusing to generate id for %d milliseconds" % abs(timestamp - self.last_timestamp))  
-        if timestamp == self.last_timestamp:  
-            self.sequence = (self.sequence + 1) & 4095  
-            if self.sequence == 0:  
-                timestamp = self.wait_for_next_millis(self.last_timestamp)  
-        else:  
-            self.sequence = 0  
-        self.last_timestamp = timestamp  
-        return ((timestamp - 1288834974657) << 22) | (self.data_center_id << 17) | (self.worker_id << 12) | self.sequence  
-  
-    def wait_for_next_millis(self, last_timestamp):  
-        timestamp = int(time.time() * 1000)  
-        while timestamp <= last_timestamp:  
+    def next_id(self):
+        with self.lock:  # 加锁保证线程安全
             timestamp = int(time.time() * 1000)  
+            if timestamp < self.last_timestamp:  
+                raise Exception("Clock moved backwards. Refusing to generate id")  
+                
+            if timestamp == self.last_timestamp:  
+                self.sequence = (self.sequence + 1) & 4095  # 4095 = 2^12-1
+                if self.sequence == 0:  # 当前毫秒序列号用尽
+                    timestamp = self.wait_for_next_millis()  
+            else:  
+                self.sequence = 0  
+                
+            self.last_timestamp = timestamp  
+            return ((timestamp - 1288834974657) << 22) | (self.data_center_id << 17) | (self.worker_id << 12) | self.sequence  
+  
+    def wait_for_next_millis(self):
+        timestamp = int(time.time() * 1000)
+        while timestamp <= self.last_timestamp:
+            time.sleep(0.001)  # 休眠1毫秒
+            timestamp = int(time.time() * 1000)
         return timestamp
+
+# _SNOWFLAKE_INSTANCE = Snowflake(worker_id, data_center_id) # 需要使用单例模式生成雪花算法实例，安全可靠
